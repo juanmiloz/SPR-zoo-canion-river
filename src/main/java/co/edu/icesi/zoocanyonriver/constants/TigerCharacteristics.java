fcb7472bb2e7d59696fa430bd4a1db8e701eebd5
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2022-09-23 18:39:57 -0500

    add comment

--- src/main/java/co/edu/icesi/zoocanyonriver/constants/TigerCharacteristics.java
+++ src/main/java/co/edu/icesi/zoocanyonriver/constants/TigerCharacteristics.java
@@ -5,17 +5,17 @@ import lombok.Getter;
 
 public class TigerCharacteristics {
 
-    //WEIGHT
+    //WEIGHT (In cm)
     public final static int MALE_MAX_WEIGHT = 310;
     public final static  int MALE_MIN_WEIGHT = 1;
     public final static  int FEMALE_MAX_WEIGHT = 170;
     public final static  int FEMALE_MIN_WEIGHT = 1;
 
-    //AGE
+    //AGE (In years)
     public final static  int MAX_AGE = 26;
     public final static  int MIN_AGE = 0;
 
-    //HEIGHT
+    //HEIGHT (In cm)
     public final static  int MAX_HEIGHT = 122;
     public final static  int MIN_HEIGHT = 10;
 }
